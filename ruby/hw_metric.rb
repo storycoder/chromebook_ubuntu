ce9170152d746cc7fbb9926458a7346f9a2dfba8
Joseph Margolis <joseph.margolis@gmail.com>
2015-05-08 11:19:37 -0400

    1st height weight us can

---
+++ ruby/hw_metric.rb
@@ -0,0 +1,34 @@
+puts "Height/Weight US/Canada converter"
+puts "Are you from the US or Canada?"
+from = gets.chomp.upcase
+while from != "CANADA" || from != "US"
+  puts "That is not a valid country. Please try again."
+  puts "Are you from the US or Canada?"
+  from = gets.chomp.upcase
+  if
+  from == "US"
+  puts "What is your height in inches?"
+  height_us = gets.chomp.to_i
+  height = height_us * 2.54
+  puts "What do you weigh in pounds?"
+  weight_us = gets.chomp.to_i
+  weight = weight_us * 0.453592
+  puts "When you're in Canada, you weigh #{weight.to_i} kilograms and you stand at #{height.to_i} centimeters."
+  elsif from != "US" && from == "CANADA"
+  puts "What is your height in centimeters?"
+  height_eh = gets.chomp.to_i
+  height = height_eh * 0.393701
+  puts "what is your weight in kilograms?"
+  weight_eh = gets.chomp.to_i
+  weight = weight_eh * 2.20462
+  puts "When you're in the US, you weigh #{weight.to_i} pounds and you stand at #{height.to_i} inches."
+  end
+  break
+end
+
+  
+
+
+#   puts "That is not a valid country. Please try again."
+#   from == gets.chomp.upcase
+# end
