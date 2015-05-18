e816fbf5dfc133e4a5facfff6d31467bf939b459
Joseph Margolis <joseph.margolis@gmail.com>
2015-05-17 21:28:59 -0400

    Next

--- rails/MovieDB/db/schema.rb
+++ rails/MovieDB/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150512131924) do
+ActiveRecord::Schema.define(version: 20150514150355) do
 
   create_table "actors", force: :cascade do |t|
     t.string   "name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20150512131924) do
     t.integer  "actor_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "liked"
   end
 
 end
