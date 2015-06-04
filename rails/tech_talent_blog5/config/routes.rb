ee45f27775dcec0d20d08e54d7a473abebe6f59c
Joseph Margolis <joseph.margolis@gmail.com>
2015-06-04 11:19:12 -0400

    thru 6/4 classwork

--- rails/tech_talent_blog5/config/routes.rb
+++ rails/tech_talent_blog5/config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   resources :comments
   resources :blog_posts
 
-  root to: "home#index"
+  root to: "blog_posts#index"
+  
+  get 'your_posts' => 'blog_posts#your_posts'
+  get 'user_posts' => 'blog_posts#user_posts'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
